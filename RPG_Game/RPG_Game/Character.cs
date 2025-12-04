5ccba37740b0cbef5ff030f2f388ad4ffbc721fb
evilscaramouche <evilscaramouche@gmail.com>
2025-12-04 12:06:54 +0200

    Create Character.cs

---
+++ RPG_Game/RPG_Game/Character.cs
@@ -0,0 +1,63 @@
+﻿namespace RPG_Game
+{
+    public abstract class Character
+    {
+        private string _name;
+        private int _health;
+        private int _maxHealth;
+        private int _strength;
+
+        public string Name
+        {
+            get => _name;
+            protected set => _name = value;
+        }
+
+        public int Health
+        {
+            get => _health;
+            protected set => _health = Math.Max(0, Math.Min(value, _maxHealth));
+        }
+
+        public int MaxHealth
+        {
+            get => _maxHealth;
+            protected set => _maxHealth = value;
+        }
+
+        public int Strength
+        {
+            get => _strength;
+            protected set => _strength = value;
+        }
+
+        public bool IsAlive => Health > 0;
+
+        protected Character(string name, int health, int strength)
+        {
+            _name = name;
+            _maxHealth = health;
+            _health = health;
+            _strength = strength;
+        }
+
+        public abstract void Attack(Character target);
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Name} takes {damage} damage. Remaining health: {Health}/{MaxHealth}");
+
+            if (!IsAlive)
+            {
+                Console.WriteLine($"{Name} has been defeated!");
+            }
+        }
+        public void Heal(int amount)
+        {
+            Health += amount;
+            Console.WriteLine($"{Name} heals {amount} health. Current health: {Health}/{MaxHealth}");
+        }
+    }
+
+}
