afa45f2cee9ab9d35534acb40997f39582610a06
evilscaramouche <evilscaramouche@gmail.com>
2025-12-11 11:47:51 +0200

    new files

---
+++ RPG_Game/RPG_Game/Enemy.cs
@@ -0,0 +1,32 @@
+﻿namespace RPG_Game
+{
+    public abstract class Enemy : Character
+    {
+        private int experienceReward;
+        private List<Item> loot;
+
+        public int ExperienceReward
+        {
+            get => experienceReward;
+            protected set => experienceReward = value;
+        }
+
+        protected Enemy(string name, int health, int strength, int expReward)
+            : base(name, health, strength)
+        {
+            experienceReward = expReward;
+            loot = new List<Item>();
+        }
+
+        protected void AddLoot(Item item)
+        {
+            loot.Add(item);
+        }
+
+        public List<Item> GetLoot()
+        {
+            return new List<Item>(loot);
+        }
+    }
+
+}
