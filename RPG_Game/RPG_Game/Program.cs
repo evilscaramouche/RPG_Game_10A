42da18e7a2ff4cd29bf35d164245f62d0fafdca8
evilscaramouche <evilscaramouche@gmail.com>
2025-12-11 11:52:39 +0200

    1

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,10 +1,60 @@
 ﻿namespace RPG_Game
 {
-    public interface IEquippable
+
+    public class Player : Character, ISpellCaster
     {
-        string Name { get; }
-        void Equip();
-        void Unequip();
+        private int _mana;
+        private int _maxMana;
+        private int _experience;
+        private int _level;
+        private IEquatable<Player> _equatableWeapon;
+        private List<Item> _inventory;
+
+        public int Mana
+        {
+            get => _mana;
+            private set => _mana = Math.Max(0, Math.Min(value, _maxMana));
+        }
+
+        public int MaxMana
+        {
+            get => _maxMana;
+            private set => _maxMana = value;
+        }
+
+        public int Experience
+        {
+            get => _experience;
+            private set => _experience = value;
+        }
+
+        public int Level
+        {
+            get => _level;
+            private set => _level = value;
+        }
+
+        public IReadOnlyList<Item> Inventory => _inventory.AsReadOnly();
+        public Player(string name, int health, int strength) : base(name, health, strength)
+        {
+        }
+        
+        public int Mana => throw new NotImplementedException();
+
+        public override void Attack(Character target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CastSpell(Character target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RestoreMana(int amount)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     internal class Program
--- RPG_Game/RPG_Game/obj/Debug/net8.0/RPG_Game.AssemblyInfo.cs
+++ RPG_Game/RPG_Game/obj/Debug/net8.0/RPG_Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a6297edfb698cb18a537523f77344607e7e795")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
